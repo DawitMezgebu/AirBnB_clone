c407b5d766d7ccd01e0b653fe7ce9e15ecdc5f98
Seife Shiferaw <102078042+siluye@users.noreply.github.com>
2023-02-14 21:20:20 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,13 +6,13 @@ Unittest classes:
     TestBaseModel_save
     TestBaseModel_to_dict
 """
+
 import os
 import models
 import unittest
 from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
-
 
 class TestBaseModel_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the BaseModel class."""
